9049c7ebd3db0e16c998b68a22bda2d61d0b5df2
joey906 <jtf1owataeba2ymi@gmail.com>
2022-02-21 21:33:38 +0900

    change

--- sql/create_Blogdata.sql
+++ sql/create_Blogdata.sql
@@ -3,7 +3,7 @@ create table Blogdata(
     title Varchar(191),
     content text,
     category int,
-    post_at timestamp,
+    post_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     published_status int default 1,
     primary key (id)
 );
--- test.php
+++ test.php
@@ -6,4 +6,8 @@ $word = 'test';
 $ls = split($word);
 print_r($ls);
 
+$dbs = "mysql:host=database-1.c6axyvexmzqt.ap-northeast-1.rds.amazonaws.com;dbname=testdb;charset=utf8";
+  $user='admin';
+  $password='mydbpass';
+
 ?>
